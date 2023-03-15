d148cbb33680d3280ade1dff25a7db9857e142c6
pywzzz <1021640832@qq.com>
2023-03-15 22:01:36 +0800

    p11~p13： 1.注册了1个全局组件TypeNav，6个局部组件ListContainer、Recommend、Rank、Like、Floor、Brand。

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 // 引入路由
 import router from "@/router";
+// 下面两行将三级联动组件注册为全局组件
+import TypeNav from "@/pages/Home/TypeNav";
+// 第一个参数是全局组件的名字，第二个是标明哪一个组件
+Vue.component(TypeNav.name,TypeNav)
 
 Vue.config.productionTip = false;
 
