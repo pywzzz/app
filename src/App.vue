794d844c86329d1562a991a24a74a31df0a90778
pywzzz <1021640832@qq.com>
2023-03-15 16:18:19 +0800

    p7： 1.利用v-show和$route的meta属性实现路由到Login、Register组件时不显示Footer组件。

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
         <Hearder></Hearder>
         <!-- 路由组件出口的地方位置 -->
         <router-view></router-view>
-        <Footer></Footer>
+        <!-- 实现路由到Login、Register组件时不显示Footer组件 -->
+        <!-- 这儿不用v-if，因为它会实际操作DOM，性能不好 -->
+        <Footer v-show="$route.meta.isShow"></Footer>
     </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -11,26 +11,32 @@ import Login from "@/pages/Login";
 export default new VueRouter({
     routes: [
         // 如果path是xxx，则呈现component中写的组件
+        /* 这儿的path、component、meta云都是配置对象，是人设计的，你在这儿瞎写的不会生效
+        如瞎写个 test: XXX 则在$route上也是没有test属性的 */
         {
             path: "/home",
             component: Home,
+            meta: { isShow: true },
         },
         {
             path: "/search",
             component: Search,
+            meta: { isShow: true },
         },
         {
             path: "/register",
             component: Register,
+            meta: { isShow: false },
         },
         {
             path: "/login",
             component: Login,
+            meta: { isShow: false },
         },
         //path的*即访问/，也即项目第一次启动，让其重定向到Home组件，即首页
         {
-            path:"*",
-            redirect:"/home"
-        }
+            path: "*",
+            redirect: "/home",
+        },
     ],
 });
