7c0a1a590e13049fb38ea55dcc014d8ebd6ea762
pywzzz <1021640832@qq.com>
2023-03-16 11:16:50 +0800

    p17： 1.引入了个进度条，更好看些。

--- src/api/request.js
+++ src/api/request.js
@@ -1,5 +1,8 @@
 // 二次封装axios就是为了使用它的请求和响应拦截器
 import axios from "axios";
+// 引入进度条及其样式
+import nprogress from "nprogress";
+import "nprogress/nprogress.css";
 
 /* requests是axios的实例对象
 创建一个实例对象的好处就是可以对axios进行一些配置 */
@@ -15,14 +18,19 @@ const requests = axios.create({
 
 //这是请求拦截器。在发送请求之前，它可以对其进行检测，从而在请求被发送前做些事
 requests.interceptors.request.use((config) => {
+    // 进度条开始
+    nprogress.start();
     // 这个config是拦截器的一个配置对象，它的一个叫headers的属性可以让我们拿到请求头
     return config;
 });
 
 //这是响应拦截器
 requests.interceptors.response.use(
-    // 第一个是成功时的回调函数，返回服务器响应的数据
+    // 第一个是成功时的回调函数
     (res) => {
+        // 进度条结束
+        nprogress.done();
+        // 返回服务器响应的数据
         return res.data;
     },
     // 第二个是失败时的回调函数
