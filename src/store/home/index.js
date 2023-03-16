faab0b0874e478254c1a117051d383c95a1dc8b6
pywzzz <1021640832@qq.com>
2023-03-16 23:03:20 +0800

    p20： 1.从服务器获取数据，动态展示了三级列表的数据。 2.使home仓库从服务器动态获取数据。

--- src/store/home/index.js
+++ src/store/home/index.js
@@ -1,9 +1,26 @@
+import { reqCategoryList } from "@/api";
 // 仓库存数据的地方
-const state = {};
+const state = {
+    // 因为服务器返回的数据是数组，所以相对应地，这儿的初始值也应为数组
+    categoryList: [],
+};
 // 修改state的唯一地方
-const mutations = {};
+const mutations = {
+    CATEGORYLIST(state, categoryList) {
+        state.categoryList = categoryList;
+    },
+};
 // 书写业务逻辑，处理异步云
-const actions = {};
+const actions = {
+    //通过API中的接口函数调用，向服务发送请求获取服务器的数据
+    // async和await是同时存在的
+    async categoryList({ commit }) {
+        let result = await reqCategoryList();
+        if (result.code == 200) {
+            commit("CATEGORYLIST", result.data);
+        }
+    },
+};
 // 功能类似计算属性，用于少些写东西
 const getters = {};
 
