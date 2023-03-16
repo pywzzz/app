91405facba23ef8cbf49cebddff4b5645bbca61b
pywzzz <1021640832@qq.com>
2023-03-16 16:19:16 +0800

    p19： 1.把TypeNav这些个全局组件放到components文件夹下。 2.把之前测试axios用的代码删了。

--- src/main.js
+++ src/main.js
@@ -5,15 +5,13 @@ import router from "@/router";
 // 引入仓库
 import store from "@/store";
 // 下面两行将三级联动组件注册为全局组件
-import TypeNav from "@/pages/Home/TypeNav";
+import TypeNav from "@/components/TypeNav";
 // 第一个参数是全局组件的名字，第二个是标明哪一个组件
 Vue.component(TypeNav.name, TypeNav);
 
 Vue.config.productionTip = false;
 
-//这儿因为reqCategoryList是分别暴露，所以记得加{}
-import { reqCategoryList } from "@/api";
-reqCategoryList();
+
 
 new Vue({
     render: (h) => h(App),
