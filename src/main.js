cd3b52b0701b798c7f1a36f6a3ea6aac1ed39384
pywzzz <1021640832@qq.com>
2023-03-16 09:08:28 +0800

    p16： 1.测试下再处理跨域问题后reqCategoryList()方法是否可以收到服务器返回的数据。

--- src/main.js
+++ src/main.js
@@ -5,9 +5,12 @@ import router from "@/router";
 // 下面两行将三级联动组件注册为全局组件
 import TypeNav from "@/pages/Home/TypeNav";
 // 第一个参数是全局组件的名字，第二个是标明哪一个组件
-Vue.component(TypeNav.name,TypeNav)
+Vue.component(TypeNav.name, TypeNav);
 
 Vue.config.productionTip = false;
+
+import { reqCategoryList } from "@/api";
+reqCategoryList();
 
 new Vue({
     render: (h) => h(App),
