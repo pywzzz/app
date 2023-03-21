9c08de0c20673e23e1b6dea78b922cefc2fcae94
pywzzz <1021640832@qq.com>
2023-03-21 21:05:32 +0800

    66~67： 1.实现了轮播图和放大镜这俩东西。 2.实现了点击缩略图后，在轮播图区域展示相应图片的功能。 3.期间利用全局事件总线实现兄弟间传参。

--- src/pages/Detail/ImageList/ImageList.vue
+++ src/pages/Detail/ImageList/ImageList.vue
@@ -1,12 +1,17 @@
 <template>
-    <div class="swiper-container">
+    <div class="swiper-container" ref="cur">
         <div class="swiper-wrapper">
             <div
                 class="swiper-slide"
-                v-for="slide in skuImageList"
+                v-for="(slide, index) in skuImageList"
                 :key="slide.id"
             >
-                <img :src="slide.imgUrl" />
+            <!-- 用currentIndex这个数据控制active样式 -->
+                <img
+                    :src="slide.imgUrl"
+                    :class="{ active: currentIndex == index }"
+                    @click="changeCurrentIndex(index)"
+                />
             </div>
         </div>
         <div class="swiper-button-next"></div>
@@ -20,6 +25,36 @@ export default {
     name: "ImageList",
     // 接收父组件Detail传的数据
     props: ["skuImageList"],
+    data() {
+        return {
+            currentIndex: 0,
+        };
+    },
+    methods: {
+        changeCurrentIndex(index) {
+            this.currentIndex = index;
+            // 向兄弟组件Zoom传叫getIndex的数据，它表示用户点的第几张图
+            this.$bus.$emit("getIndex", this.currentIndex);
+        },
+    },
+    watch: {
+        // 下面是弄轮播图相关
+        skuImageList() {
+            this.$nextTick(() => {
+                new Swiper(this.$refs.cur, {
+                    // 如果需要前进后退按钮
+                    navigation: {
+                        nextEl: ".swiper-button-next",
+                        prevEl: ".swiper-button-prev",
+                    },
+                    // 轮播图那个框中显示
+                    slidesPerView: 3,
+                    // 点击一下轮播图会出来几个新的图
+                    slidesPerGroup: 1,
+                });
+            });
+        },
+    },
 };
 </script>
 
@@ -44,11 +79,6 @@ export default {
             display: block;
 
             &.active {
-                border: 2px solid #f60;
-                padding: 1px;
-            }
-
-            &:hover {
                 border: 2px solid #f60;
                 padding: 1px;
             }
--- src/pages/Detail/Zoom/Zoom.vue
+++ src/pages/Detail/Zoom/Zoom.vue
@@ -3,13 +3,14 @@
         <!-- 这个是轮播图框中的图 -->
         <!-- 默认展示第一张，故 skuImageList[0] -->
         <img :src="imgObj.imgUrl" />
-        <div class="event"></div>
+        <div class="event" @mousemove="handler"></div>
         <div class="big">
-            <!-- 这个是放大镜放大后展示的图 -->
+            <!-- 这个是利用big样式实现的放大镜放大后，展示的图 -->
             <!-- 默认展示第一张，故 skuImageList[0]  -->
-            <img :src="imgObj.imgUrl" />
+            <img :src="imgObj.imgUrl" ref="big" />
         </div>
-        <div class="mask"></div>
+        <!-- 放大镜的那个遮罩层 -->
+        <div class="mask" ref="mask"></div>
     </div>
 </template>
 
@@ -18,11 +19,49 @@ export default {
     name: "Zoom",
     // 收到父组件Detail传来的参数
     props: ["skuImageList"],
+    data() {
+        return {
+            // 0代表第一张轮播图的缩略图有active样式，即高亮
+            currentIndex: 0,
+        };
+    },
     computed: {
         imgObj() {
             // 这儿也是类比search仓库之解决因为undefined导致的报红
-            return this.skuImageList[0] || {};
+            // 默认展示第一张，即skuImageList[0]，这儿的this.currentIndex默认值你弄的也是0
+            return this.skuImageList[this.currentIndex] || {};
         },
+    },
+    methods: {
+        handler(event) {
+            let mask = this.$refs.mask;
+            let big = this.$refs.big;
+            let left = event.offsetX - mask.offsetWidth / 2;
+            let top = event.offsetY - mask.offsetHeight / 2;
+            // 约束遮罩层的范围
+            if (left <= 0) {
+                left = 0;
+            } else if (left >= mask.offsetWidth) {
+                left = mask.offsetWidth;
+            } else if (top <= 0) {
+                top = 0;
+            } else if (top >= mask.offsetHeight) {
+                top = mask.offsetHeight;
+            }
+            // 这儿赋值时别忘了加 "px" 这东西
+            mask.style.left = left + "px";
+            mask.style.top = top + "px";
+            // 这儿的乘2是因为你下面的css中的.big下的img弄的是200%，所以这儿应该乘2
+            // 这的负号是因为你移动的是图片，你向左移动，对于图片就是向右，所以是负号
+            big.style.left = -2 * left + "px";
+            big.style.top = -2 * top + "px";
+        },
+    },
+    mounted() {
+        // 接收兄弟组件ImageList传过来的数据getIndex数据，它表示用户点击的第几张图
+        this.$bus.$on("getIndex", (index) => {
+            this.currentIndex = index;
+        });
     },
 };
 </script>
