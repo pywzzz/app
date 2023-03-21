94ebb64283487f67b7803d0245ea64923261fb06
pywzzz <1021640832@qq.com>
2023-03-21 11:40:24 +0800

    59： 1.添加了个路由跳转的滚动行为。

--- src/router/index.js
+++ src/router/index.js
@@ -8,4 +8,9 @@ Vue.use(VueRouter);
 export default new VueRouter({
     // kv一致省略v
     routes,
+    // 这是vue的路由跳转时的滚动行为，
+    scrollBehavior(to, from, savedPosition) {
+        // 下面表示路由跳转到新页面后，新页面的滚动条在y轴的0处，即最上方。
+        return { y: 0 };
+    },
 });
