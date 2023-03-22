0bfd0de2c023f682962b6b774abc00f1fef67983
pywzzz <1021640832@qq.com>
2023-03-22 23:20:53 +0800

    75： 1.获取shopcart仓库中的数据，并在ShopCart组件中动态展示。

--- src/store/shopcart/index.js
+++ src/store/shopcart/index.js
@@ -1,16 +1,30 @@
 import { reqCartList } from "@/api";
 // 仓库存数据的地方
-const state = {};
+const state = {
+    cartList: [],
+};
 // 修改state的唯一地方
-const mutations = {};
+const mutations = {
+    GETCARTLIST(state, cartList) {
+        state.cartList = cartList;
+    },
+};
 // 书写业务逻辑，处理异步云
 const actions = {
     async getCartList({ commit }) {
         let result = await reqCartList();
+        if (result.code == 200) {
+            commit("GETCARTLIST", result.data);
+        }
     },
 };
 // 功能类似计算属性，用于少些写东西
-const getters = {};
+const getters = {
+    cartList(state) {
+        // 这儿 || 的是个 {} 是因为，算了你自己看人返回的数据格式就知道了
+        return state.cartList[0] || {};
+    },
+};
 
 // 对外暴露Store类的一个实例对象
 export default {
