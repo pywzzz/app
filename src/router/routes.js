8f12b4d7a4f8d2a8662e610b20be51da1f3d9617
pywzzz <1021640832@qq.com>
2023-03-26 11:47:17 +0800

    108： 1.将路由组件的引入方式换成了路由懒加载云。

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,18 +1,3 @@
-// 引入路由组件
-import Home from "@/pages/Home";
-import Search from "@/pages/Search";
-import Register from "@/pages/Register";
-import Login from "@/pages/Login";
-import Detail from "@/pages/Detail";
-import AddCartSuccess from "@/pages/AddCartSuccess";
-import ShopCart from "@/pages/ShopCart";
-import Trade from "@/pages/Trade";
-import Pay from "@/pages/Pay";
-import PaySuccess from "@/pages/PaySuccess";
-import Center from "@/pages/Center";
-// 引入二级路由组件
-import Myorder from "@/pages/Center/MyOrder";
-import GroupOrder from "@/pages/Center/GroupOrder";
 // 路由配置信息
 export default [
     // 如果path是xxx，则呈现component中写的组件
@@ -20,12 +5,12 @@ export default [
     如瞎写个 test: XXX 则在$route上也是没有test属性的 */
     {
         path: "/home",
-        component: Home,
+        component: () => import("@/pages/Home"),
         meta: { isShow: true },
     },
     {
         path: "/center",
-        component: Center,
+        component: () => import("@/pages/Center"),
         meta: { isShow: true },
         // 访问 /center 会重定向到 /center/myorder
         redirect: "/center/myorder",
@@ -33,11 +18,11 @@ export default [
         children: [
             {
                 path: "myorder",
-                component: Myorder,
+                component: () => import("@/pages/Center/MyOrder"),
             },
             {
                 path: "grouporder",
-                component: GroupOrder,
+                component: () => import("@/pages/Center/GroupOrder"),
             },
         ],
     },
@@ -48,29 +33,29 @@ export default [
         /* 即如果你不加的话，当你访 localhost:8080/#/home/search?canshu=444 ，因为你没有传param参数，
         所以实际的地址栏上会显示 localhost:8080/#/home/?canshu=444 ，且你的路由也并不会跳转 */
         path: "/search/:keyword?",
-        component: Search,
+        component: () => import("@/pages/Search"),
         meta: { isShow: true },
     },
     {
         // 从Search跳转到相应的Detail要提供skuid这个params参数
         path: "/detail/:skuid",
-        component: Detail,
+        component: () => import("@/pages/Detail"),
         meta: { isShow: true },
     },
     {
         name: "addcartsuccess",
         path: "/addcartsuccess",
-        component: AddCartSuccess,
+        component: () => import("@/pages/AddCartSuccess"),
         meta: { isShow: true },
     },
     {
         path: "/shopcart",
-        component: ShopCart,
+        component: () => import("@/pages/ShopCart"),
         meta: { isShow: true },
     },
     {
         path: "/trade",
-        component: Trade,
+        component: () => import("@/pages/Trade"),
         meta: { isShow: false },
         // 路由独享守卫只负责，路由跳转至如这儿的 /trade 时的情况，即只对自己负责
         beforeEnter: (to, from, next) => {
@@ -87,7 +72,7 @@ export default [
     },
     {
         path: "/pay",
-        component: Pay,
+        component: () => import("@/pages/Pay"),
         meta: { isShow: false },
         beforeEnter: (to, from, next) => {
             // 正常情况下进入 /pay 的上一个页面是 /trade
@@ -103,17 +88,17 @@ export default [
     },
     {
         path: "/paysuccess",
-        component: PaySuccess,
+        component: () => import("@/pages/PaySuccess"),
         meta: { isShow: false },
     },
     {
         path: "/register",
-        component: Register,
+        component: () => import("@/pages/Register"),
         meta: { isShow: false },
     },
     {
         path: "/login",
-        component: Login,
+        component: () => import("@/pages/Login"),
         meta: { isShow: false },
     },
     //path的*即访问/，也即项目第一次启动，让其重定向到Home组件，即首页
